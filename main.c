33ec09a06afc466fba6c3ce63b6cf1cf18757615
GiulioBiagini <bella.biagio@hotmail.it>
2016-04-24 15:05:06 +0200

    Versione parallela con MPI

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+
+#include "mpi.h"
 
 #include "entity/view.h"
 #include "environment.h"
@@ -7,102 +10,122 @@
 
 
 
-/* map */
-
 const int MAP_WIDTH = 10;
 
 const int MAP_HEIGHT = 10;
 
 const int CANS_NUMBER = 10;
 
-/* population */
+const int VIEW_TYPE = UNCOLLABORATIVE_CROSS_VIEW;
 
 const int PAIRS_NUMBER = 1;
 
-const view_type_t VIEW_TYPE = COLLABORATIVE_CROSS_VIEW;
-
-/* evolution */
-
-const float MUTATION_PROBABILITY = 0.005;
-
-/* simulation */
-
 const int GENERATIONS_NUMBER = 1;
 
-const int SESSIONS_NUMBER = 5;
+const int SESSIONS_NUMBER = 10;
 
-const int ACTIONS_PER_SESSION_NUMBER = 1;
+const int ACTIONS_PER_SESSION_NUMBER = 10;
 
-/* seed */
+const int MUTATION_PROBABILITY = 0.005;
 
-const int SIMULATION_SEED = 10;
+const int SEED = 10;
 
 
 
-/* main */
+#define SEND_PAIR(pair, dest)\
+	MPI_Send((pair)->robby_1->dna, (pair)->robby_1->dna_size, MPI_INT, dest, 666, MPI_COMM_WORLD);\
+	MPI_Send((pair)->robby_2->dna, (pair)->robby_2->dna_size, MPI_INT, dest, 666, MPI_COMM_WORLD)
+
+#define RECV_PAIR(pair, src)\
+	MPI_Recv((pair)->robby_1->dna, (pair)->robby_1->dna_size, MPI_INT, src, 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE);\
+	MPI_Recv((pair)->robby_2->dna, (pair)->robby_2->dna_size, MPI_INT, src, 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE)
+
+#define SEND_FITNESS(fitness_value, dest)\
+	MPI_Send(fitness_value, 1, MPI_INT, dest, 777, MPI_COMM_WORLD)
+
+#define RECV_FITNESS(fitness_value, src)\
+	MPI_Recv(fitness_value, 1, MPI_INT, src, 777, MPI_COMM_WORLD, MPI_STATUS_IGNORE)
+
+
 
 int main(int argc, char **argv) {
-	int g;
-	int p;
-	int s;
-	int a;
+	int my_id;
+	int processes_number;
 	
-	environment_t *env;
-	population_t *tmp;
-	population_t *population;
-	population_t *new_population;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+	MPI_Comm_size(MPI_COMM_WORLD, &processes_number);
 	
-	srand(SIMULATION_SEED);
+	srand(SEED);
 	
-	env = allocate_environment(MAP_WIDTH, MAP_HEIGHT, CANS_NUMBER);
-	population = allocate_population(VIEW_TYPE, PAIRS_NUMBER);
-	new_population = allocate_population(VIEW_TYPE, PAIRS_NUMBER);
-	init_random_population(population);
-	
-	/* for all generations */
-	for (g = 0; g < GENERATIONS_NUMBER; g++) {
-		/* for each pair */
-		for (p = 0; p < population->pairs_number; p++) {
-			/* set pair into the environment and reset fitness value */
-			env->pair = &(population->pairs[p]);
-			env->pair->fitness_value = 0;
-			/* for each cleanin session */
-			for (s = 0; s < SESSIONS_NUMBER; s++) {
-				/* reset pair fitness, init random map and robbies position */
-				init_random_environment(env);
-				/* for each action */
-				for (a = 0; a < ACTIONS_PER_SESSION_NUMBER; a++) {
-					/* update robbies views, relative actions and perform them */
-#ifdef DEBUG
-					printf(
-						"%s\nGENERATION %d/%d - PAIR %d/%d - SESSION %d/%d - ACTION %d/%d\n",
-						"-------------------------------------------------------",
-						(g + 1), GENERATIONS_NUMBER,
-						(p + 1), population->pairs_number,
-						(s + 1), SESSIONS_NUMBER,
-						(a + 1), ACTIONS_PER_SESSION_NUMBER
-					);
-#endif
-					execute_step(env);
-				}
+	if (my_id == 0) {
+		int g;
+		int p;
+		int dest;
+		population_t *population;
+		population_t *new_population;
+		population_t *tmp;
+		
+		/* allocate population */
+		population = allocate_population(VIEW_TYPE, PAIRS_NUMBER);
+		new_population = allocate_population(VIEW_TYPE, PAIRS_NUMBER);
+		
+		/* init population with random dna */
+		init_random_population(population);
+		
+		for (g = 0; g < GENERATIONS_NUMBER; g++) {
+			/* send pairs to processes for evaluation */
+			for (p = 0; p < population->pairs_number; p++) {
+				dest = (p % (processes_number - 1)) + 1;
+				SEND_PAIR(&(population->pairs[p]), dest);
 			}
-			/* calculate fitness average */
-			env->pair->fitness_value /= SESSIONS_NUMBER;
-#ifndef DEBUG
-			printf(
-				"GENERATION %d/%d - PAIR %d/%d -> %f\n",
-				(g + 1), GENERATIONS_NUMBER,
-				(p + 1), population->pairs_number,
-				env->pair->fitness_value
-			);
-#endif
+			/* receive fitness values from processes */
+			for (p = 0; p < population->pairs_number; p++) {
+				dest = (p % (processes_number - 1)) + 1;
+				RECV_FITNESS(&(population->pairs[p].fitness_value), dest);
+				printf(
+					"GENERATION %d/%d, PAIR %d/%d, FITNESS: %f\n",
+					(g + 1), GENERATIONS_NUMBER,
+					(p + 1), PAIRS_NUMBER,
+					population->pairs[p].fitness_value
+				);
+			}
+			/* evolve population */
+			evolve(population, new_population, MUTATION_PROBABILITY);
+			tmp = population;
+			population = new_population;
+			new_population = tmp;
 		}
-		/* evolve population */
-		evolve(population, new_population, MUTATION_PROBABILITY);
-		tmp = population;
-		population = new_population;
-		new_population = tmp;
+	} else {
+		int p;
+		int s;
+		int a;
+		pair_t *pair;
+		environment_t *env;
+		
+		/* allocate pair and environment */
+		pair = allocate_pair(VIEW_TYPE);
+		env = allocate_environment(MAP_WIDTH, MAP_HEIGHT, CANS_NUMBER);
+		
+		for (p = 0; p < PAIRS_NUMBER; p++) {
+			/* p is my pair */
+			if ((p % (processes_number - 1)) + 1 == my_id) {
+				/* receive pair for evaluation */
+				RECV_PAIR(pair, 0);
+				/* evaluate pair */
+				pair->fitness_value = 0;
+				for (s = 0; s < SESSIONS_NUMBER; s++) {
+					init_random_environment(env);
+					for (a = 0; a < ACTIONS_PER_SESSION_NUMBER; a++)
+						execute_step(env);
+				}
+				/* send fitness value */
+				SEND_FITNESS(&(pair->fitness_value), 0);
+			}
+		}
 	}
+	
+	MPI_Finalize();
 	
 	return 0;
 }
